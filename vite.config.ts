036394ab69af6e5afe8e159900f02e6ca4c09602
NikolinaPetkovic <nikolinapetkovic001@gmail.com>
2025-07-09 15:44:34 +0200

    Build for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // <-- OVO je tačno!
+import react from '@vitejs/plugin-react';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: './', 
   plugins: [react()],
   resolve: {
     alias: {
